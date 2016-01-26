836e7b7056b104206edb09fe9b4d092c13f5cca7
Manuel Rueda <manuel.rueda.un@gmail.com>
2016-01-25 22:08:56 -0300

    fix cloud flare script

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -9,15 +9,6 @@ if [ "$NEWIP" = "$CURRENTIP" ]
 then
   echo "IP address unchanged"
 else
-  curl https://www.cloudflare.com/api_json.html \
-    -d 'a=rec_edit' \
-    -d 'tkn=$CF_DDNS_TKN' \
-    -d 'email=$CF_DDNS_EMAIL' \
-    -d 'z=$CF_DDNS_DOMAIN' \
-    -d 'id=$CF_DDNS_REC_ID' \
-    -d 'type=A' \
-    -d 'name=$CF_DDNS_NAME' \
-    -d 'ttl=1' \
-    -d "content=$NEWIP"
+  curl "https://www.cloudflare.com/api_json.html?a=rec_edit&tkn=$CF_DDNS_TKN&email=$CF_DDNS_EMAIL&z=$CF_DDNS_DOMAIN&id=$CF_DDNS_REC_ID&type=A&name=$CF_DDNS_NAME&ttl=1&content=$NEWIP"
   echo $NEWIP > /var/tmp/currentip.txt
 fi
